bbf4f2e7945f9a25bbd303139dcef0fa46bc9f27
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-08-13 11:49:38 -0700

    Updated settings

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+import streamlit as st
+from langchain.llms import OpenAI
 
-apikey = os.getenv('OPENAI_API_KEY')
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
+
+# App framework
+st.title('🦜️🔗 YouTube GPT Creator')
+prompt = st.text_input('Enter your prompt here')
+
+#LLMs
+llm = OpenAI(temperature=0.9)
+
+# Show stuff to the screen if there is one
+if prompt:
+    response = llm(prompt)
+    # render back to streamlit
+    st.write(response)

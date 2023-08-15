4022ddb735f35deeddfe2582e24031a7fab80283
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-08-14 19:14:05 -0700

    daily app update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
@@ -11,11 +13,18 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 st.title('🦜️🔗 YouTube GPT Creator')
 prompt = st.text_input('Enter your prompt here')
 
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables=['topic'],
+    template='write me a youtube video title about deep {topic}'
+)
+
 #LLMs
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
 
 # Show stuff to the screen if there is one
 if prompt:
-    response = llm(prompt)
+    response = title_chain.run(prompt)
     # render back to streamlit
     st.write(response)

2d2bd9beeed196fe7bd1613ea0057105507fb9bc
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-08-15 14:56:26 -0700

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
@@ -19,12 +19,19 @@ title_template = PromptTemplate(
     template='write me a youtube video title about deep {topic}'
 )
 
+script_template = PromptTemplate(
+    input_variables=['title'],
+    template='write me a youtube video script based on this title TITLE: {title}'
+)
+
 #LLMs
 llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
+sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain])
 
 # Show stuff to the screen if there is one
 if prompt:
-    response = title_chain.run(prompt)
+    response = sequential_chain.run(prompt)
     # render back to streamlit
     st.write(response)

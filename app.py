b665de22357b0c35515c220186eb33ce8cb0076b
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-09-02 18:46:58 -0700

    sequential chain updates

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
@@ -16,7 +17,7 @@ prompt = st.text_input('Enter your prompt here')
 # Prompt templates
 title_template = PromptTemplate(
     input_variables=['topic'],
-    template='write me a youtube video title about deep {topic}'
+    template='write me a youtube video title about {topic}'
 )
 
 script_template = PromptTemplate(
@@ -26,12 +27,16 @@ script_template = PromptTemplate(
 
 #LLMs
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain])
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+sequential_chain = SequentialChain(chains=[title_chain, script_chain],
+                                   input_variables=['topic'],
+                                   output_variables=['title', 'script'],
+                                   verbose=True)
 
 # Show stuff to the screen if there is one
 if prompt:
-    response = sequential_chain.run(prompt)
+    response = sequential_chain({'topic': prompt})
     # render back to streamlit
-    st.write(response)
+    st.write(response['title'])
+    st.write(response['script'])
